ad2d649a2f03422cd61e82ea95a0c8d1b5dfb5b3
Apolosmllb <minermllb@gmail.com>
2022-11-30 19:11:39 -0500

    Update ScoreRespository.java

--- src/main/java/com/ecodrive/platform/u/si729ebu201920785/behaviour/domain/persistence/ScoreRespository.java
+++ src/main/java/com/ecodrive/platform/u/si729ebu201920785/behaviour/domain/persistence/ScoreRespository.java
@@ -7,12 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ScoreRespository extends JpaRepository<Score,Long> {
-
     @Query("SELECT  AVG (s.value )  FROM Score s WHERE s.driverId =?1")
     Double getAverage(Long driverId);
-
     @Query("SELECT  MAX(s.value )  FROM Score s WHERE s.driverId =?1")
     Double getMax(Long driverId);
-
-
 }
