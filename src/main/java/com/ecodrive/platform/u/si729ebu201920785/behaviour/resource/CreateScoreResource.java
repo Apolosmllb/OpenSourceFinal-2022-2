c5fc9090dc26332cfaa2d72e88045f6f39937489
Apolosmllb <minermllb@gmail.com>
2022-11-30 19:14:32 -0500

    Update CreateScoreResource.java

--- src/main/java/com/ecodrive/platform/u/si729ebu201920785/behaviour/resource/CreateScoreResource.java
+++ src/main/java/com/ecodrive/platform/u/si729ebu201920785/behaviour/resource/CreateScoreResource.java
@@ -1,10 +1,8 @@
 package com.ecodrive.platform.u.si729ebu201920785.behaviour.resource;
 
 import lombok.*;
-
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.math.BigDecimal;
+
 
 @Getter
 @Setter
