8728aafcf49e3ac2f1a0f61fc30102ace3c24e1f
Apolosmllb <minermllb@gmail.com>
2022-11-30 19:14:20 -0500

    Update ScoreResource.java

--- src/main/java/com/ecodrive/platform/u/si729ebu201920785/behaviour/resource/ScoreResource.java
+++ src/main/java/com/ecodrive/platform/u/si729ebu201920785/behaviour/resource/ScoreResource.java
@@ -2,14 +2,6 @@ package com.ecodrive.platform.u.si729ebu201920785.behaviour.resource;
 
 
 import lombok.*;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.math.BigDecimal;
 import java.util.Date;
 
 @Getter
